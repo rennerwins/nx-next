03b0e5fd0af4daa6631a0f97692340288ae56569
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2022-06-27 21:06:35 +0700

    setup lint

--- apps/nx-next/pages/index.tsx
+++ apps/nx-next/pages/index.tsx
@@ -1,7 +1,11 @@
+const arr = ['h', 'c', 'e', 'a'];
+
 export function Index() {
   return (
     <div>
-      <h1>Hello</h1>
+      {arr.map((d, i) => (
+        <h1 key={d}>{d}</h1>
+      ))}
     </div>
   );
 }
