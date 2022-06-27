99d5b22edbb79c5f06493a678861e155aeb39dc9
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2022-06-27 20:43:48 +0700

    fix precommit

--- apps/nx-next/pages/index.tsx
+++ apps/nx-next/pages/index.tsx
@@ -1,44 +1,9 @@
-import Link from 'next/link';
-import { GetStaticProps } from 'next/types';
-import { dehydrate, QueryClient, useQuery } from 'react-query';
-import { useTheme } from 'next-themes';
-
-const getPokemons = async () => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon');
-  const data = await res.json();
-  return data.results;
-};
-
 export function Index() {
-  const { data } = useQuery('pokemons', getPokemons);
-  const { setTheme } = useTheme();
-
   return (
-    <div className="bg-background">
-      <button>Change theme</button>
-
-      <ul>
-        {data.map((pokemon) => (
-          <Link href={`/pokemon/${pokemon.name}`} key={pokemon.name}>
-            <a>
-              <li>{pokemon.name}</li>
-            </a>
-          </Link>
-        ))}
-      </ul>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery('pokemons', getPokemons);
-
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-};
 
 export default Index;
